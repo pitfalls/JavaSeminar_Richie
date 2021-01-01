cf76c688342a54aa9d09db781e54713ffc86b733
SchallerlRichard <richard.delsen@gmail.com>
2021-01-01 15:36:20 +0100

    something bla bla

---
+++ Java Seminar/src/App.java
@@ -0,0 +1,17 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+
+
+        int a = 10;
+        int b = 10;
+        int result = a + b;
+        
+    System.out.println("solution " + result);
+
+
+
+
+
+    }
+}
